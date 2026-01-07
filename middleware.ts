387acb82767496a85157954037c3fad31895b7ad
sexyoung <Habuche@gmail.com>
2026-01-07 14:51:35 +0800

    fix: don't need GATEWAY_URL :sheep:

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export default function middleware(request: Request) {
   const url = new URL(request.url);
   const { pathname, search } = url;
 
-  const targetHost = process.env.GATEWAY_URL || 'https://yumyumapi-gateway-staging.up.railway.app';
+  const targetHost = process.env.VITE_API_URL || 'https://yumyumgame-service-staging.up.railway.app';
   const destination = new URL(`${targetHost}${pathname}${search}`);
 
   // 官方「純 Web API」做法：回傳一個帶有特定 Header 的 Response
